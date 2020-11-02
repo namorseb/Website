10d88c6f40149d1c51a954680a516cc1090d373e
Sebastian <namorseb@gmail.com>
2020-11-02 00:12:34 -0600

    changed static dir

--- distribution/src/src/settings.py
+++ distribution/src/src/settings.py
@@ -136,7 +136,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static_in_env'),
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media_root')
