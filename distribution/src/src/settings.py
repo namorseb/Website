69851cdb8a5401b8761efd67c72ef5fa1a64f31b
Sebastian <namorseb@gmail.com>
2020-11-02 02:06:13 -0600

    added personal images to static files

--- distribution/src/src/settings.py
+++ distribution/src/src/settings.py
@@ -76,7 +76,15 @@ WSGI_APPLICATION = 'src.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
+else:
+    DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql_psycopg2',
             'NAME': 'website',
