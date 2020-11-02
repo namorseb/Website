a39e39cc7d96f0711db88fdae1966369a5c52f0d
Sebastian <namorseb@gmail.com>
2020-11-02 00:41:21 -0600

    changed static dir

--- distribution/src/src/settings.py
+++ distribution/src/src/settings.py
@@ -75,15 +75,8 @@ WSGI_APPLICATION = 'src.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-if DEBUG:
-    DATABASES = {
-    'default': {
-       'ENGINE': 'django.db.backends.sqlite3',
-       'NAME': os.path.join(BASE_DIR,'db.sqlite3'),
-       }
-  }
-else:
-    DATABASES = {
+
+DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql_psycopg2',
             'NAME': 'website',
